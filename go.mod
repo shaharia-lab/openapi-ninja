d7336f4debfb0fb4bc051baf39097292e41770a4
Shaharia Azam <mail@shaharia.com>
2023-04-16 20:47:52 +0200

    Added more router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shahariaazam/openapi-ninja
 
-go 1.20
+go 1.18
+
+require github.com/go-chi/chi/v5 v5.0.8
--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-	http.HandleFunc("/api/ping", pingHandler)
+	r := chi.NewRouter()
+	r.Get("/ping", pingHandler)
+
+	r.Get("/api/what-is-my-ip", myIPHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -17,11 +22,34 @@ func main() {
 	}
 
 	log.Printf("Listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	if err := http.ListenAndServe(":"+port, r); err != nil {
 		log.Fatal(err)
 	}
 }
 
 func pingHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Pong")
+}
+
+func myIPHandler(w http.ResponseWriter, r *http.Request) {
+	ip := r.Header.Get("X-Forwarded-For")
+	if ip == "" {
+		ip = r.RemoteAddr
+	}
+
+	format := r.URL.Query().Get("format")
+	switch format {
+	case "json":
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, `{"ip": "%s"}`, ip)
+	case "text":
+		w.Header().Set("Content-Type", "text/plain")
+		fmt.Fprintf(w, "%s", ip)
+	case "xml":
+		w.Header().Set("Content-Type", "application/xml")
+		fmt.Fprintf(w, "<ip>%s</ip>", ip)
+	default:
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintf(w, `{"ip": "%s"}`, ip)
+	}
 }
